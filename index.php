122fda401604135eee5fceac7f5ff7fadd59ccc9
andmade <ashnichdavis@gmail.com>
2016-09-10 19:48:24 -0400

    Added base HTML

--- index.php
+++ index.php
@@ -1,1 +1,49 @@
-<h1>This is where p1 will go.</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>ashley davis | dwa15 | fall 2016</title>
+
+    <!--CSS-->
+    <link rel="stylesheet" href="css/foundation.min.css">
+
+    <!--Javascript-->
+    <script src="js/vendor/foundation.min.js"></script>
+
+</head>
+
+<body>
+    <div class="row expanded medium-unstack">
+        <div class="columns row">
+            <section>
+                <h2>Ashley Davis</h2>
+                <a href="#">Live Site</a>
+                <a href="#">Github Repo</a>
+            </section>
+        </div>
+        <div class="columns row">
+            <section>
+                <h2>P2| TBD</h2>
+                <a href="#">Live Site</a>
+                <a href="#">Github Repo</a>
+            </section>
+        </div>
+        <div class="columns row">
+            <section>
+                <h2>P3 | TBD</h2>
+                <a href="#">Live Site</a>
+                <a href="#">Github Repo</a>
+            </section>
+        </div>
+        <div class="columns row">
+            <section>
+                <h2>P4 | TBD</h2>
+                <a href="#">Live Site</a>
+                <a href="#">Github Repo</a>
+            </section>
+        </div>
+    </div>
+</body>
+
+</html>
