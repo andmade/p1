cf62fb790e23c41139dce61b94029db6b4333633
andmade <ashnichdavis@gmail.com>
2016-09-15 13:20:26 -0400

    Add responsive css/html
    Finish text
    
    Change bio picture and add placeholder screenshot image

--- index.php
+++ index.php
@@ -3,14 +3,19 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>ashley davis | dwa15 | fall 2016</title>
 
     <!--CSS-->
     <link rel="stylesheet" href="css/foundation.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css" />
+
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Baloo+Tamma|Open+Sans">
     <link rel="stylesheet" href="css/p1.css">
 
     <!--Javascript-->
+    <script src="https://code.jquery.com/jquery-2.2.3.min.js" type="text/javascript"></script>
+
     <script src="js/vendor/foundation.min.js"></script>
 
 </head>
@@ -18,38 +23,60 @@
 <body>
 
     <header>
-        <h1>DWA 15 | Project Portfolio</h1>
+        <h1>DWA 15</h1>
     </header>
 
     <div class="row expanded medium-unstack">
-        <div id="bio-section" class="columns">
-            <section>
-                <img src="biopic.jpg" />
-                <h1>Ashley Davis</h1>
-                <a href="#">Live Site</a>
-                <a href="#">Github Repo</a>
+        <div id="bio-section" class="columns medium-2">
+            <section class="row">
+                <div class="column small-6 medium-12">
+                    <img src="biopic2.png" />
+                </div>
+                <div class="column small-6 medium-12">
+                    <h1>Ashley Davis</h1>
+                    <a href="https://twitter.com/andmademe"><i class="fa fa-twitter-square" aria-hidden="true"></i> @andmademe</a>
+                    <a href="https://github.com/andmade"><i class="fa fa-github-square" aria-hidden="true"></i> @andmade</a>
+                </div>
             </section>
         </div>
         <div class="columns">
-
-            <section>
-                <h1>P2| TBD</h1>
-                <a href="#">Live Site</a>
-                <a href="#">Github Repo</a>
+            <h1>Project Portfolio</h1>
+            <section class="row ">
+                <div class="column small-5 medium-12">
+                    <img src="placeholder.png" />
+                </div>
+                <div class="column small-7 medium-12">
+                    <h1>P2 | xkcd password gen</h1>
+                    <h2></h2>
+                    <a href="#">Live Site</a>
+                    <a href="#">Github Repo</a>
+                </div>
             </section>
         </div>
         <div class="columns">
-            <section>
-                <h1>P3 | TBD</h1>
-                <a href="#">Live Site</a>
-                <a href="#">Github Repo</a>
+            <section class="row">
+                <div class="column small-5 medium-12">
+                    <img src="placeholder.png" />
+                </div>
+                <div class="column small-7 medium-12">
+                    <h1>P3 | Dev's Best Friend</h1>
+                    <h2></h2>
+                    <a href="#">Live Site</a>
+                    <a href="#">Github Repo</a>
+                </div>
             </section>
         </div>
         <div class="columns">
-            <section>
-                <h1>P4 | TBD</h1>
-                <a href="#">Live Site</a>
-                <a href="#">Github Repo</a>
+            <section class="row">
+                <div class="column small-5 medium-12">
+                    <img src="placeholder.png" />
+                </div>
+                <div class="column small-7 medium-12">
+                    <h1>P4 | To Be Determined!</h1>
+                    <h2></h2>
+                    <a href="#">Live Site</a>
+                    <a href="#">Github Repo</a>
+                </div>
             </section>
         </div>
     </div>
